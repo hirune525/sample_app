e701b846aec39436ffa5b7a40d55ec728aae6b20
Tabata Ryo <hirune525@gmail.com>
2015-05-16 15:08:56 +0900

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,7 +10,7 @@ describe "Static Pages" do
     before { visit root_path }
 
     it { should have_content('Sample App') }
-    it { should have_title("#{base_title}") }
+    it { should have_title(full_title('')) }
     it { should_not have_title(' | Home') }
 
   end
@@ -19,7 +19,7 @@ describe "Static Pages" do
     before { visit help_path }
 
     it { should have_content('Help') }
-    it { should have_title("#{base_title} | Help")}
+    it { should have_title(full_title('Help'))}
 
   end
 
@@ -27,7 +27,7 @@ describe "Static Pages" do
     before { visit about_path }
 
     it { should have_content('About Us')}
-    it { should have_title("#{base_title} | About Us")}
+    it { should have_title(full_title('About Us'))}
 
   end
 
@@ -35,7 +35,7 @@ describe "Static Pages" do
     before { visit contact_path }
 
     it { should have_content('Contact') }
-    it { should have_title("#{base_title} | Contact") }
+    it { should have_title(full_title('Contact')) }
 
   end
 
