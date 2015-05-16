1c02af76de240aa289dc26e84f5c5ff381e2f762
Tabata Ryo <hirune525@gmail.com>
2015-05-17 00:16:04 +0900

    Add user model

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+
+  # 保存前にActiveRecodeから呼び出してもらいemailをすべて小文字にして保存
+  before_save { self.email = email.downcase }
+
+  # 存在(presence), 文字長検証, メール形式
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+end
