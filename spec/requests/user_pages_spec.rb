dd5f49b5452fb62636ae81a9f7efb866967ff1e1
Tabata Ryo <hirune525@gmail.com>
2015-05-16 15:37:46 +0900

    modified spec

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,12 +3,17 @@ require 'spec_helper'
 describe "User pages" do
 
   subject { page }
+  shared_examples_for "all user pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
 
   describe "signup page" do
     before { visit signup_path }
+    let(:heading)    { 'Sign up'}
+    let(:page_title) { 'Sign up' }
 
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up'))}
+    it_should_behave_like "all user pages"
   end
 
 end
